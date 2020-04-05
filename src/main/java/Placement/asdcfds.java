60ddf8ea17512bc26b7b78aef5a75b022737cde1
Oscar Pindaro <oscar.pindaro@gmail.com>
2020-04-05 19:31:42 +0200

    added uml

--- src/main/java/Placement/asdcfds.java
+++ src/main/java/Placement/asdcfds.java
@@ -1,4 +1,20 @@
 package Placement;
 
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Node;
+import io.kubernetes.client.proto.Meta;
+
 public class asdcfds {
+
+    public static void main(String[] args) {
+        V1Node node = new V1Node();
+
+        CoreV1Api api = new CoreV1Api();
+
+        api.app
+
+        ApiClient client = new ApiClient();
+        client.
+    }
 }
