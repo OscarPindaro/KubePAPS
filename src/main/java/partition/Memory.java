51b0268402fe999218e1bbcd8245b4c80af50f68
FabioLosavio <fabio.losavio@hotmail.it>
2020-04-10 17:07:43 +0200

    added function to compute the label to spread and other functions needed

--- src/main/java/partition/Memory.java
+++ src/main/java/partition/Memory.java
@@ -1,6 +1,8 @@
 package partition;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Memory {
@@ -28,9 +30,7 @@ public class Memory {
         this.totLabelReceived = totLabelReceived +1;
     }
 
-    public Map<String, Integer> getMemory() {
-        return memory;
-    }
+    public Map<String, Integer> getMemory() { return new HashMap<String, Integer>(memory); }
 
     public void updateMemory(String label) {
         int previousValue;
@@ -45,26 +45,43 @@ public class Memory {
         this.updateTotLabelReceived();
     }
 
+    public int getOccurrences(String label) { return memory.get(label); }
 
-    //functions
-    public float computeLabelReturnProbability(String label) {
 
-        float labelOccurrence = this.memory.get(label);
+    // FUNCTIONS
 
-        return (labelOccurrence/this.totLabelReceived);
-
+    // this function returns a list of all the labels occurring in the memory
+    // sorted by number of occurrences and sorted in ascending order
+    public List<String> returnSortedLabels() {
+        List<String> sortedList;
+        sortedList = new ArrayList<>(this.memory.keySet());
+        sortedList.sort((a,b)->{
+            if (memory.get(a) > memory.get(b)) {
+                return 1;
+            }
+            else if(memory.get(a).equals(memory.get(b))){
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        });
+        return sortedList;
     }
 
-// debug test
-/*    public static void main(String[] args) {
+
+/*    // debug test
+    public static void main(String[] args) {
         Memory memoriaProva = new Memory("fabio");
         System.out.println(memoriaProva.getMemory());
         memoriaProva.updateMemory("fabio");
         memoriaProva.updateMemory("oscar");
         memoriaProva.updateMemory("oscar");
+        memoriaProva.updateMemory("oscar");
         memoriaProva.updateMemory("enka");
         System.out.println(memoriaProva.getMemory());
         System.out.println(memoriaProva.getTotLabelReceived());
+        System.out.println(memoriaProva.returnSortedLabels());
 
     }*/
 }
