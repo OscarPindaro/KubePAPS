c2db299a03a674a84c8f439c69afab73e526dc8f
Oscar Pindaro <oscar.pindaro@gmail.com>
2020-04-10 15:48:10 +0200

    added the method that extract an object based on its occurency

--- src/main/java/partition/SPLA_Node.java
+++ src/main/java/partition/SPLA_Node.java
@@ -1,5 +1,7 @@
 package partition;
 
+import java.util.Random;
+
 public class SPLA_Node {
 
     private String nodeID;
@@ -7,4 +9,15 @@ public class SPLA_Node {
     private String currentLabel;       //label currently selected by the node, set up checking the memory
     private Memory memory;      //used to save all the labels received by other nodes
 
+    public void extractLabel(){
+        int gaussianSum = individuals.size()*(individuals.size()-1)/2;
+        int selectionValue = new Random().nextInt(gaussianSum);
+        int sum= 0;
+        int pos= 0;
+        while( sum < selectionValue){
+            pos++;
+            sum += pos;
+        }
+        return new Individual(individuals.get(pos-1));
+    }
 }
